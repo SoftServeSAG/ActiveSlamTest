307eae7ebee0f4ab53146ef05911695c62df4e23
DmytroKushnir <kushnir_d@ucu.edu.ua>
2019-03-31 17:47:08 +0000

    Files renamed according to classes used or intended to use

---
+++ ast_explore_lite/include/explore/Visualiser.h
@@ -0,0 +1,8 @@
+/*
+ * This class is created to encapsulate all visualisation issues for AST_frontier_explore package
+ *
+ * */
+#ifndef SRC_VISUALISER_H
+#define SRC_VISUALISER_H
+
+#endif //SRC_VISUALISER_H
